5044dcfce64d3be5b5f5e93dfd21e04beac8a4fd
akshaysrivastav <akshaysrivastav2014@gmail.com>
2020-10-22 09:13:56 +0530

    deploy script - transfer of reward token added

--- scripts/deploy-masterchef.js
+++ scripts/deploy-masterchef.js
@@ -10,6 +10,7 @@ const rl = require("./common/rl");
 
 const MasterChef = artifacts.require("MasterChef");
 const Proxy = artifacts.require("Proxy");
+const IERC20 = artifacts.require("IERC20");
 
 module.exports = async (callback) => {
   try {
@@ -24,16 +25,18 @@ module.exports = async (callback) => {
     let accounts = await web3.eth.getAccounts();
     console.log(`\nUsing account: ${accounts[0]}\n`);
 
+    let latestBlock = await web3.eth.getBlock('latest');
+    console.log(`\nLatest Block: ${latestBlock.number}\n`);
+
     let openToken = await promisify(rl.question)("Provide OPEN Token address: ") || contractAddresses.OPEN;
     let devAddress = await promisify(rl.question)("Provide Dev Address: ") || accounts[0];
     let perBlockReward = await promisify(rl.question)("Provide per block reward amount(not in Wei): ") || '100';
-    
+
     let startBlock = await promisify(rl.question)("Provide reward distribution start block: ");
     if (!startBlock) {
-      let latestBlock = await web3.eth.getBlock('latest');
       startBlock = latestBlock.number;
     }
-    
+
     let bonusEndBlock = await promisify(rl.question)("Provide reward distribution bonus end block: ");
     if (!bonusEndBlock) {
       bonusEndBlock = parseFloat(startBlock) + 10000;
@@ -46,7 +49,7 @@ module.exports = async (callback) => {
     }
 
     perBlockReward = web3.utils.toWei(perBlockReward);
-  
+
     let txParams = {
       gasPrice: await setGasPrice(world, network)
     }
@@ -54,7 +57,7 @@ module.exports = async (callback) => {
     console.log(`\nDeploying MasterChef Logic...`);
     // Deploy MasterChef logic/implementation
     let logic = await MasterChef.new(txParams);
-    console.log(`\nMasterChef Logic deployed at: `, logic.address);
+    console.log(`\nMasterChef Logic deployed at: ${logic.address}`);
     console.log(`\nDeploying MasterChef Proxy...`);
 
     // Deploy MasterChef itself as proxy
@@ -69,12 +72,26 @@ module.exports = async (callback) => {
     const proxy = await Proxy.new(masterChefConstructCode, logic.address, txParams);
     console.log(`\nMasterChef proxy deployed at: ${proxy.address}`);
 
+    const OpenToken = await IERC20.at(contractAddresses.OPEN);
+    const openBalance = await OpenToken.balanceOf(accounts[0]);
+    console.log(`\nAccount's OPEN balance: ${web3.utils.fromWei(openBalance, 'ether')}`);
+
+    let amount = await promisify(rl.question)("Provide amount of OPEN to transfer to MasterChef: ");
+    if (!(parseFloat(amount) > 0)) {
+      console.log('\nTranfer Aborted');
+      callback();
+    }
+
+    await OpenToken.transfer(proxy.address, web3.utils.toWei(amount));
+    console.log(`\nTransferred ${amount} OPEN to Proxy at ${proxy.address}`);
+
     callback();
   } catch (err) {
     callback(err);
   }
 };
 
+// TODO: should move it to 'utils'?
 const setGasPrice = async (world, network) => {
   let gasPrice = await getGasPrice(world);
   if (network === 'mainnet' && process.env.MAINNET_GAS_PRICE) {
